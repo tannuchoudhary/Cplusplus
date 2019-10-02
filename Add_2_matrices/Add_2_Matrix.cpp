2a57697d79bf5c52b6cb687691de9c744d6835fc
TANNU KUMARI <tannuchoudhary10@gmail.com>
2019-10-02 12:51:14 +0530

    Update Add_2_Matrix.cpp

--- Add_2_matrices/Add_2_Matrix.cpp
+++ Add_2_matrices/Add_2_Matrix.cpp
@@ -7,26 +7,26 @@ void main()
 
    cout<<"Enter the number of rows(should be >1 and <10): ";
    cin>>r;
-   cout<<"Enter the number of column(should be >1 and <10): ";
+   cout<<"Enter the number of columns(should be >1 and <10): ";
    cin>>c;
-   cout << "Enter the elements of first 1st matrix: ";
-   for (int i = 0;i<r;i++ ) {
-     for (int j = 0;j < c;j++ ) {
+   cout << "Enter all the elements of first matrix: "<<endl;
+   for (int i = 0; i < r; i++ ) {
+     for (int j = 0; j < c; j++ ) {
        cin>>m1[i][j];
      }
    }
-   cout << "Enter the elements of first 1st matrix: ";
-   for (int i = 0;i<r;i++ ) {
-     for (int j = 0;j<c;j++ ) {
+   cout << "Enter all the elements of second matrix: "<<endl;
+   for (int i = 0; i < r; i++ ) {
+     for (int j = 0; j < c; j++ ) {
        cin>>m2[i][j];
      }
    }
 
    cout<<"Output: ";
-   for (int i = 0;i<r;i++ ) {
-      for (int j = 0;j<c;j++ ) {
-        sum[i][j]=m1[i][j]+m2[i][j];
-        cout<<sum[i][j]<<" ";
+   for (int i = 0; i < r; i++ ) {
+      for (int j = 0; j < c; j++ ) {
+        sum[i][j] = m1[i][j] + m2[i][j];
+        cout<<sum[i][j]<<" "; 
       }
    }
 
